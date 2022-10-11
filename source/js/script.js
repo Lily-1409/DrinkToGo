63231cca4b3d3092a7d641d5ea17f3925a88a9f7
Аникина Лилия <zolina.liliya@gmail.com>
2022-10-12 00:50:36 +0300

    project

---
+++ source/js/script.js
@@ -0,0 +1,56 @@
+/* import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination'; */
+
+/* Swiper.use([Navigation, Pagination]); */
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+const map = L.map('map')
+.on('load', () => {
+  console.log('Карта инициализирована');
+})
+.setView({
+  lat: 59.92749,
+  lng: 30.31127,
+}, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const marker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+);
+
+marker.addTo(map);
+
+const buttonMenu = document.querySelector('.main-nav-button');
+const menu = document.querySelector('.main-nav__list');
+
+buttonMenu.addEventListener('click', function () {
+  menu.classList.toggle('main-nav__list--active');
+  buttonMenu.classList.toggle('main-nav-button--active');
+});
